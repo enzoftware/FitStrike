55e18d243780306c00315358f9df10c66f28fc81
Enzo Lizama <lizama.enzo@gmail.com>
2024-08-19 21:45:53 -0500

    Initial Commit

---
+++ FitStrike/FitStrikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitStrikeApp.swift
+//  FitStrike
+//
+//  Created by Enzo Lizama on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FitStrikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
