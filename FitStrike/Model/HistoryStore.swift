07620b6cf89458ba29838c874891fee8c699f8e3
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-02 11:21:56 -0500

    chore: turn history store into env object

--- FitStrike/Model/HistoryStore.swift
+++ FitStrike/Model/HistoryStore.swift
@@ -6,14 +6,22 @@ struct ExerciseDay: Identifiable {
     var exercises: [String] = []
 }
 
-struct HistoryStore {
+class HistoryStore: ObservableObject {
+    @Published var exerciseDays: [ExerciseDay] = []
+
     init() {
         #if DEBUG
         createDevData()
         #endif
     }
 
-    var exerciseDays: [ExerciseDay] = []
+    func addDoneExercise(_ exercise: String) {
+        let today = Date()
+        if today.isSameDay(as: exerciseDays[0].date) {
+            debugPrint("Adding \(exercise)")
+            exerciseDays[0].exercises.append(exercise)
+        } else {
+            exerciseDays.insert(ExerciseDay(date: today, exercises: [exercise]), at: 0)
+        }
+    }
 }
-
-
