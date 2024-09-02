693379a2a17b00b5d4a105dfc8f4730639dfb192
Enzo Lizama <lizama.enzo@gmail.com>
2024-09-02 14:21:45 -0500

    use scene storage and app storage

--- FitStrike/Views/RatingView.swift
+++ FitStrike/Views/RatingView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct RatingView: View {
-    @Binding var rating: Int
+    let exerciseIndex: Int
+
+    /// Here you hold rating locally and set up ratings to be a string of four zeros.
+    @AppStorage("ratings") private var storageRatings = ""
+
+    @State private var rating = 0
 
     let maximumRating = 5
 
     let onColor = Color.red
     let offColor = Color.gray
+
+    init(exerciseIndex: Int) {
+        self.exerciseIndex = exerciseIndex
+        let desireLength = Exercise.exercises.count
+        if storageRatings.count < desireLength {
+            self.storageRatings = storageRatings.padding(
+                toLength: desireLength,
+                withPad: "0",
+                startingAt: 0
+            )
+        }
+    }
+
+    func updateRating(index: Int) {
+        rating = index
+        let index = storageRatings.index(storageRatings.startIndex, offsetBy: exerciseIndex)
+        storageRatings.replaceSubrange(index ... index, with: String(rating))
+    }
+
+    fileprivate func convertRating() {
+        let index = storageRatings.index(storageRatings.startIndex, offsetBy: exerciseIndex)
+        let character = storageRatings[index]
+        rating = character.wholeNumberValue ?? 0
+    }
 
     var body: some View {
         HStack {
@@ -24,8 +53,14 @@ struct RatingView: View {
 
                 Image(systemName: "waveform.path.ecg")
                     .foregroundColor(color)
+                    .onChange(of: storageRatings) { _, _ in
+                        convertRating()
+                    }
+                    .onAppear {
+                        convertRating()
+                    }
                     .onTapGesture {
-                        rating = index
+                        updateRating(index: index)
                     }
             }.font(.largeTitle)
         }
@@ -33,5 +68,5 @@ struct RatingView: View {
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
-    RatingView(rating: .constant(3))
+    RatingView(exerciseIndex: 0)
 }
