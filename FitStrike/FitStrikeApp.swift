f957668c519863eef38115e7b92f4239bb1338dd
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2024-09-16 13:56:58 -0500

    Saving `HistoryStore` data 💾  (#7)
    * chore: show alert for not loaded file
    
    * feat: load information from history.plist

--- FitStrike/FitStrikeApp.swift
+++ FitStrike/FitStrikeApp.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 @main
 struct FitStrikeApp: App {
+    @StateObject private var historyStore = HistoryStore()
+
     var body: some Scene {
         WindowGroup {
             ContentView().onAppear(perform: {
                 debugPrint(URL.documentsDirectory)
+            }).alert(isPresented: $historyStore.loadingError, content: {
+                Alert(title: Text("History"), message: Text(
+                    """
+                    Unfortunately we can't load your past history.
+                    Email support:
+                      support@xyz.com
+                    """))
             })
-        }
+        }.environmentObject(historyStore)
     }
 }
--- FitStrike/Model/HistoryStore.swift
+++ FitStrike/Model/HistoryStore.swift
@@ -7,21 +7,78 @@ struct ExerciseDay: Identifiable {
 }
 
 class HistoryStore: ObservableObject {
+    enum FileError: Error {
+        case loadFailure
+        case saveFailure
+    }
+
     @Published var exerciseDays: [ExerciseDay] = []
+    @Published var loadingError = false
 
     init() {
-        #if DEBUG
-        createDevData()
-        #endif
+        do {
+            try load()
+        } catch {
+            loadingError = true
+            print("Error:", error)
+        }
+    }
+
+    var dataUrl: URL {
+        URL.documentsDirectory.appendingPathComponent("history.plist")
     }
 
     func addDoneExercise(_ exercise: String) {
         let today = Date()
-        if today.isSameDay(as: exerciseDays[0].date) {
+        if let firstDate = exerciseDays.first?.date, today.isSameDay(as: firstDate) {
             debugPrint("Adding \(exercise)")
             exerciseDays[0].exercises.append(exercise)
         } else {
             exerciseDays.insert(ExerciseDay(date: today, exercises: [exercise]), at: 0)
+        }
+
+        do {
+            try save()
+            print("History: ", exerciseDays)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    func load() throws {
+        guard let data = try? Data(contentsOf: dataUrl) else {
+            return
+        }
+        do {
+            let plistData = try PropertyListSerialization.propertyList(
+                from: data,
+                options: [],
+                format: nil)
+
+            let convertedPlistData = plistData as? [[Any]] ?? []
+
+            exerciseDays = convertedPlistData.map {
+                ExerciseDay(
+                    date: $0[1] as? Date ?? Date(),
+                    exercises: $0[2] as? [String] ?? [])
+            }
+        } catch {
+            throw FileError.loadFailure
+        }
+    }
+
+    func save() throws {
+        let plistData = exerciseDays.map {
+            [$0.id.uuidString, $0.date, $0.exercises]
+        }
+        do {
+            let data = try PropertyListSerialization.data(
+                fromPropertyList: plistData,
+                format: .binary,
+                options: .zero)
+            try data.write(to: dataUrl, options: .atomic)
+        } catch {
+            throw FileError.saveFailure
         }
     }
 }
